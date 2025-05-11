8b47b651cd8c29588de07b11c3758cd65aaaeba4
DovMac <dmpsfkld@gmail.com>
2025-05-11 21:04:20 +0300

    Create BoardsController.cs(Multiple To-do-list boards)
    Multiple To-do-list boards

---
+++ api/Controllers/BoardsController.cs
@@ -0,0 +1,162 @@
+// Controllers/BoardsController.cs
+// Multiple To-do-list boards
+
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoListApp.Data;
+using TodoListApp.Models;
+
+namespace TodoListApp.Controllers
+{
+    public class BoardsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BoardsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Boards
+        public async Task<IActionResult> Index()
+        {
+            var boards = await _context.Boards
+                .OrderByDescending(b => b.CreatedAt)
+                .ToListAsync();
+            
+            return View(boards);
+        }
+
+        // GET: Boards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Boards
+                .Include(b => b.TodoItems)
+                .FirstOrDefaultAsync(m => m.Id == id);
+                
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // GET: Boards/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Boards/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                board.CreatedAt = DateTime.Now;
+                _context.Add(board);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Boards/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Boards.FindAsync(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Boards/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Board board)
+        {
+            if (id != board.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Preserve the CreatedAt date from the original board
+                    var originalBoard = await _context.Boards.AsNoTracking().FirstOrDefaultAsync(b => b.Id == id);
+                    board.CreatedAt = originalBoard.CreatedAt;
+                    
+                    _context.Update(board);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoardExists(board.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Boards/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Boards
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // POST: Boards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var board = await _context.Boards.FindAsync(id);
+            _context.Boards.Remove(board);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoardExists(int id)
+        {
+            return _context.Boards.Any(e => e.Id == id);
+        }
+    }
+}
