f918371129e301ceabb89ad193d2af3b222fb516
DovMac <dmpsfkld@gmail.com>
2025-05-24 20:21:05 +0300

    Update Board.cs

--- api/Models/Board.cs
+++ api/Models/Board.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using MyBackend.Models;
 
 namespace TodoListApp.Models
